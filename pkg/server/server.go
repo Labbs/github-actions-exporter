6899c92635a0259421604f7c7beb140d02d291b2
labbs <moutonjeremy@labbs.fr>
2021-03-13 18:22:56 +0100

    add if for enable debug

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -20,11 +20,14 @@ func RunServer(ctx *cli.Context) error {
 		ctx.WriteString("/metrics")
 	})
 	r.GET("/metrics", prometheusHandler())
-	r.GET("/debug/pprof/", pprofHandlerIndex)
-	r.GET("/debug/pprof/cmdline", pprofHandlerCmdline)
-	r.GET("/debug/pprof/profile", pprofHandlerIndex)
-	r.GET("/debug/pprof/trace", pprofHandlerTrace)
-	r.GET("/debug/pprof/{profile}", pprofHandlerIndex)
+
+	if config.Debug {
+		r.GET("/debug/pprof/", pprofHandlerIndex)
+		r.GET("/debug/pprof/cmdline", pprofHandlerCmdline)
+		r.GET("/debug/pprof/profile", pprofHandlerIndex)
+		r.GET("/debug/pprof/trace", pprofHandlerTrace)
+		r.GET("/debug/pprof/{profile}", pprofHandlerIndex)
+	}
 
 	log.Print("exporter listening on 0.0.0.0:" + strconv.Itoa(config.Port))
 	return fasthttp.ListenAndServe(":"+strconv.Itoa(config.Port), r.Handler)
