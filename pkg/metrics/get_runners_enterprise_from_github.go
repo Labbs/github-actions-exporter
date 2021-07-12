94ae75fec7d6abbea198cdf797fd343c83f43912
Neill Turner <neillwturner@gmail.com>
2021-07-12 16:27:37 +0100

    fix entrprise calls
    only do github enterprise api called when EnterpriseName parameter specify.

--- pkg/metrics/get_runners_enterprise_from_github.go
+++ pkg/metrics/get_runners_enterprise_from_github.go
@@ -21,19 +21,21 @@ var (
 )
 
 func getRunnersEnterpriseFromGithub() {
-	for {
-		runners, _, err := client.Enterprise.ListRunners(context.Background(), config.EnterpriseName, nil)
-		if err != nil {
-			log.Printf("Enterprise.ListRunners error: %s", err.Error())
-		} else {
-			for _, runner := range runners.Runners {
-				var integerStatus float64
-				if integerStatus = 0; runner.GetStatus() == "online" {
-					integerStatus = 1
+	if config.EnterpriseName != "" {
+		for {
+			runners, _, err := client.Enterprise.ListRunners(context.Background(), config.EnterpriseName, nil)
+			if err != nil {
+				log.Printf("Enterprise.ListRunners error: %s", err.Error())
+			} else {
+				for _, runner := range runners.Runners {
+					var integerStatus float64
+					if integerStatus = 0; runner.GetStatus() == "online" {
+						integerStatus = 1
+					}
+					runnersEnterpriseGauge.WithLabelValues(*runner.OS, *runner.Name, strconv.FormatInt(runner.GetID(), 10)).Set(integerStatus)
 				}
-				runnersEnterpriseGauge.WithLabelValues(*runner.OS, *runner.Name, strconv.FormatInt(runner.GetID(), 10)).Set(integerStatus)
 			}
-		}
 		time.Sleep(time.Duration(config.Github.Refresh) * time.Second)
+	}
 	}
 }
